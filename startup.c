11bb0cdb94ea2d0ce70842de9482892d59d7b087
Jakub Piotr Cłapa <jpc@loee.pl>
2010-10-17 21:35:54 +0200

    [startup] Add LPC13xx device-specific interrupts.

--- startup.c
+++ startup.c
@@ -1,7 +1,5 @@
 /*
- * ARM Cortex-M3 startup file for the NXP LPC17xx processors.
- *
- * TODO: Add vendor specific interrupt vectors.
+ * ARM Cortex-M3 startup file for the NXP LPC13xx processors.
  *
  * Copyright (c) 2010 LoEE - Jakub Piotr Cłapa
  * This program is released under the new BSD license.
@@ -23,7 +21,41 @@
   WEAK_HANDLER(DebugMon) \
   RESERVED \
   WEAK_HANDLER(PendSV) \
-  WEAK_HANDLER(SysTick)
+  WEAK_HANDLER(SysTick) \
+  /* Vendor specific interrupts for the LPC13xx: */ \
+  /* Wake-up pin interrupts (40 interrupts): */ \
+  WEAK_HANDLER(WakeUp0_0) WEAK_HANDLER(WakeUp0_1) WEAK_HANDLER(WakeUp0_2) WEAK_HANDLER(WakeUp0_3) \
+  WEAK_HANDLER(WakeUp0_4) WEAK_HANDLER(WakeUp0_5) WEAK_HANDLER(WakeUp0_6) WEAK_HANDLER(WakeUp0_7) \
+  WEAK_HANDLER(WakeUp0_8) WEAK_HANDLER(WakeUp0_9) WEAK_HANDLER(WakeUp0_10) WEAK_HANDLER(WakeUp0_11) \
+  \
+  WEAK_HANDLER(WakeUp1_0) WEAK_HANDLER(WakeUp1_1) WEAK_HANDLER(WakeUp1_2) WEAK_HANDLER(WakeUp1_3) \
+  WEAK_HANDLER(WakeUp1_4) WEAK_HANDLER(WakeUp1_5) WEAK_HANDLER(WakeUp1_6) WEAK_HANDLER(WakeUp1_7) \
+  WEAK_HANDLER(WakeUp1_8) WEAK_HANDLER(WakeUp1_9) WEAK_HANDLER(WakeUp1_10) WEAK_HANDLER(WakeUp1_11) \
+  \
+  WEAK_HANDLER(WakeUp2_0) WEAK_HANDLER(WakeUp2_1) WEAK_HANDLER(WakeUp2_2) WEAK_HANDLER(WakeUp2_3) \
+  WEAK_HANDLER(WakeUp2_4) WEAK_HANDLER(WakeUp2_5) WEAK_HANDLER(WakeUp2_6) WEAK_HANDLER(WakeUp2_7) \
+  WEAK_HANDLER(WakeUp2_8) WEAK_HANDLER(WakeUp2_9) WEAK_HANDLER(WakeUp2_10) WEAK_HANDLER(WakeUp2_11) \
+  \
+  WEAK_HANDLER(WakeUp3_0) WEAK_HANDLER(WakeUp3_1) WEAK_HANDLER(WakeUp3_2) WEAK_HANDLER(WakeUp3_3) \
+  \
+  /* Other peripherals: */ \
+  WEAK_HANDLER(I2C0) \
+  WEAK_HANDLER(CT16B0) \
+  WEAK_HANDLER(CT16B1) \
+  WEAK_HANDLER(CT32B0) \
+  WEAK_HANDLER(CT32B1) \
+  WEAK_HANDLER(SSP) \
+  WEAK_HANDLER(UART) \
+  WEAK_HANDLER(USBIRQ) \
+  WEAK_HANDLER(USBFIQ) \
+  WEAK_HANDLER(ADC) \
+  WEAK_HANDLER(WDT) \
+  WEAK_HANDLER(BOD) \
+  RESERVED \
+  WEAK_HANDLER(PIO3) \
+  WEAK_HANDLER(PIO2) \
+  WEAK_HANDLER(PIO1) \
+  WEAK_HANDLER(PIO0)
 
 // Dummy handler.
 void Default_Handler (void) { while (1); }

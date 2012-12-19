ba62a376458f62fce2032e24f5003f426399da4a
Jakub Piotr Cłapa <jpc@loee.pl>
2012-12-20 00:29:39 +0100

    Add LPC122x interrupt vectors.

--- startup.c
+++ startup.c
@@ -1,10 +1,10 @@
 /*
    ARM Cortex-M3 startup file for the NXP LPC1xxx processors.
-  
+
    For more details see:
        http://bitbucket.org/jpc/lpc1xxx-hw/
- 
-   Copyright (c) 2010-2011 LoEE - Jakub Piotr C³apa
+
+   Copyright (c) 2010-2011 LoEE - Jakub Piotr CÅ‚apa
    This program is released under the new BSD license.
  */
 #include "hw.h"
@@ -16,6 +16,23 @@
   WEAK_HANDLER(MemManage) \
   WEAK_HANDLER(BusFault) \
   WEAK_HANDLER(UsageFault) \
+  RESERVED \
+  RESERVED \
+  RESERVED \
+  RESERVED \
+  WEAK_HANDLER(SVC) \
+  WEAK_HANDLER(DebugMon) \
+  RESERVED \
+  WEAK_HANDLER(PendSV) \
+  WEAK_HANDLER(SysTick) \
+
+#define CM0_VECTORS \
+  HANDLER(Reset) \
+  WEAK_HANDLER(NMI) \
+  WEAK_HANDLER(HardFault) \
+  RESERVED \
+  RESERVED \
+  RESERVED \
   RESERVED \
   RESERVED \
   RESERVED \
@@ -100,13 +117,41 @@
   WEAK_HANDLER(PIO1) \
   WEAK_HANDLER(PIO0)
 
+#define LPC122x_VECTORS \
+  /* Vendor specific interrupts for the LPC122x: */ \
+  /* Wake-up pin interrupts (12 interrupts): */ \
+  WEAK_HANDLER(WakeUpP0_0) WEAK_HANDLER(WakeUpP0_1) WEAK_HANDLER(WakeUpP0_2) WEAK_HANDLER(WakeUpP0_3) \
+  WEAK_HANDLER(WakeUpP0_4) WEAK_HANDLER(WakeUpP0_5) WEAK_HANDLER(WakeUpP0_6) WEAK_HANDLER(WakeUpP0_7) \
+  WEAK_HANDLER(WakeUpP0_8) WEAK_HANDLER(WakeUpP0_9) WEAK_HANDLER(WakeUpP0_10) WEAK_HANDLER(WakeUpP0_11) \
+  \
+  /* Other peripherals: */ \
+  WEAK_HANDLER(I2C0) \
+  WEAK_HANDLER(CT16B0) \
+  WEAK_HANDLER(CT16B1) \
+  WEAK_HANDLER(CT32B0) \
+  WEAK_HANDLER(CT32B1) \
+  WEAK_HANDLER(SSP) \
+  WEAK_HANDLER(UART0) \
+  WEAK_HANDLER(UART1) \
+  WEAK_HANDLER(CMP) \
+  WEAK_HANDLER(ADC) \
+  WEAK_HANDLER(WDT) \
+  WEAK_HANDLER(BOD) \
+  RESERVED \
+  WEAK_HANDLER(PIO0) \
+  WEAK_HANDLER(PIO1) \
+  WEAK_HANDLER(PIO2) \
+  RESERVED \
+  WEAK_HANDLER(DMA) \
+  WEAK_HANDLER(RTC) \
+  RESERVED
+
 #if defined (LPC17xx)
 #define VECTORS CM3_VECTORS LPC17xx_VECTORS
 #elif defined (LPC13xx)
 #define VECTORS CM3_VECTORS LPC13xx_VECTORS
-#elif defined (LPC12xx)
-#define VECTORS CM3_VECTORS
-// TODO: add LPC12xx_VECTORS
+#elif defined (LPC122x)
+#define VECTORS CM0_VECTORS LPC122x_VECTORS
 #else
 #error "Unknown processor type."
 #endif
@@ -162,7 +207,7 @@ Reset_Handler(void)
   // Zero the remaining allocated RAM (.bss section)
   d = &_edata;
   do *d++ = 0; while (d < &_ebss);
-  
+
   // Everything is ready. Run the user program.
   main();
   while (1);

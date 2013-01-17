7caf13dc2b3133e4d7b529488ed3e8bcbfc3a522
Jakub Piotr Cłapa <jpc@loee.pl>
2013-01-17 17:31:53 +0100

    Add new LPC13xx CMSIS files.

--- CMSIS/LPC13xx.h
+++ CMSIS/LPC13xx.h
@@ -1,25 +1,26 @@
-/******************************************************************************
- * @file:    LPC13xx.h
- * @purpose: CMSIS Cortex-M3 Core Peripheral Access Layer Header File for 
- *           NXP LPC13xx Device Series 
- * @version: V1.0
- * @date:    25. Aug. 2009
- *----------------------------------------------------------------------------
+/****************************************************************************
+ *   LPC13xx.h - 2011-04-28
  *
- * Copyright (C) 2009 NXP Semiconductors. All rights reserved.
+ *   Based on original by NXP with modifications by Code Red Technologies
  *
- * NXP Semiconductors is supplying this software for use with Cortex-M3 
- * processor based microcontrollers.  This file can be freely distributed 
- * within development tools that are supporting such ARM based processors. 
+ *   Project: NXP LPC13xx software example
  *
- * THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- * OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- * ARM SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR
- * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ *   Description:
+ *     CMSIS Cortex-M3 Core Peripheral Access Layer Header File for
+ *     NXP LPC13xx Device Series
  *
- ******************************************************************************/
-
+ ****************************************************************************
+ * Software that is described herein is for illustrative purposes only
+ * which provides customers with programming information regarding the
+ * products. This software is supplied "AS IS" without any warranties.
+ * NXP Semiconductors assumes no responsibility or liability for the
+ * use of the software, conveys no license or title under any patent,
+ * copyright, or mask work right to the product. NXP Semiconductors
+ * reserves the right to make changes in the software without
+ * notification. NXP Semiconductors also make no representation or
+ * warranty that such application will be suitable for the specified
+ * use without further testing or modification.
+****************************************************************************/
 
 #ifndef __LPC13xx_H__
 #define __LPC13xx_H__
@@ -88,7 +89,8 @@ typedef enum IRQn
   TIMER_16_1_IRQn               = 42,       /*!< 16-bit Timer1 Interrupt                          */
   TIMER_32_0_IRQn               = 43,       /*!< 32-bit Timer0 Interrupt                          */
   TIMER_32_1_IRQn               = 44,       /*!< 32-bit Timer1 Interrupt                          */
-  SSP_IRQn                      = 45,       /*!< SSP Interrupt                                    */
+  SSP_IRQn                      = 45,       /*!< SSP0 Interrupt  - original name                                  */
+  SSP0_IRQn                     = 45,       /*!< SSP0 Interrupt  - new name                                   */
   UART_IRQn                     = 46,       /*!< UART Interrupt                                   */
   USB_IRQn                      = 47,       /*!< USB Regular Interrupt                            */
   USB_FIQn                      = 48,       /*!< USB Fast Interrupt                               */
@@ -100,6 +102,8 @@ typedef enum IRQn
   EINT2_IRQn                    = 54,       /*!< External Interrupt 2 Interrupt                   */
   EINT1_IRQn                    = 55,       /*!< External Interrupt 1 Interrupt                   */
   EINT0_IRQn                    = 56,       /*!< External Interrupt 0 Interrupt                   */
+  SSP1_IRQn                     = 57,       /*!< SSP1 Interrupt                                   */
+
 } IRQn_Type;
 
 
@@ -116,20 +120,7 @@ typedef enum IRQn
 
 
 #include "core_cm3.h"                       /* Cortex-M3 processor and core peripherals           */
-//#include "system_LPC13xx.h"                 /* System Header                                      */
-
-
-
-/**
- * Initialize the system clock
- *
- * @param  none
- * @return none
- *
- * @brief  Setup the microcontroller system.
- *         Initialize the System and update the SystemFrequency variable.
- */
-extern void SystemInit (void);
+#include "system_LPC13xx.h"                 /* System Header                                      */
 
 
 /******************************************************************************/
@@ -170,9 +161,14 @@ typedef struct
 
   __IO uint32_t SYSAHBCLKCTRL;   /* Offset 0x80 */
        uint32_t RESERVED5[4];
-  __IO uint32_t SSPCLKDIV;                 
+  union {
+       // SSP0CLKDIV preferred name rather than original SSPCLKDIV
+       __IO uint32_t SSPCLKDIV;
+       __IO uint32_t SSP0CLKDIV;
+  };
   __IO uint32_t UARTCLKDIV;
-       uint32_t RESERVED6[4];
+  __IO uint32_t SSP1CLKDIV;             /*!< Offset: 0x09C SSP1 clock divider (R/W) */
+       uint32_t RESERVED6[3];
   __IO uint32_t TRACECLKDIV;
 
   __IO uint32_t SYSTICKCLKDIV;   /* Offset 0xB0 */           
@@ -215,7 +211,7 @@ typedef struct
   __IO uint32_t PDRUNCFG;
        uint32_t RESERVED15[110];
   __I  uint32_t DEVICE_ID;
-} SYSCON_TypeDef;
+} LPC_SYSCON_TypeDef;
 
 
 /*------------- Pin Connect Block (IOCON) --------------------------------*/
@@ -250,18 +246,43 @@ typedef struct
 
   __IO uint32_t PIO0_8;
   __IO uint32_t PIO0_9;
-  __IO uint32_t JTAG_TCK_PIO0_10;
+  union {
+	  // SWCLK_PIO0_10 preferred name rather than original JTAG_TCK_PIO0_10
+	  __IO uint32_t SWCLK_PIO0_10;
+	  __IO uint32_t JTAG_TCK_PIO0_10;
+  };
   __IO uint32_t PIO1_10;
   __IO uint32_t PIO2_11;
-  __IO uint32_t JTAG_TDI_PIO0_11;
-  __IO uint32_t JTAG_TMS_PIO1_0;
-  __IO uint32_t JTAG_TDO_PIO1_1;
+  union {
+	  // R_PIO0_11 preferred name rather than original JTAG_TDI_PIO0_11
+	  __IO uint32_t R_PIO0_11;
+	  __IO uint32_t JTAG_TDI_PIO0_11;
+  };
+  union {
+	  // R_PIO1_0 preferred name rather than original JTAG_TMS_PIO1_0
+	  __IO uint32_t R_PIO1_0;
+	  __IO uint32_t JTAG_TMS_PIO1_0;
+  };
+  union {
+	  // R_PIO1_1 preferred name rather than original JTAG_TDO_PIO1_1
+	  __IO uint32_t R_PIO1_1;
+	  __IO uint32_t JTAG_TDO_PIO1_1;
+  };
 
-  __IO uint32_t JTAG_nTRST_PIO1_2;
+  union {
+	  // R_PIO1_2 preferred name rather than original JTAG_nTRST_PIO1_2
+	  __IO uint32_t R_PIO1_2;
+	  __IO uint32_t JTAG_nTRST_PIO1_2;
+  };
   __IO uint32_t PIO3_0;
   __IO uint32_t PIO3_1;
   __IO uint32_t PIO2_3;
-  __IO uint32_t ARM_SWDIO_PIO1_3;
+  union {
+	  // SWDIO_PIO1_3 preferred name rather than original ARM_SWDIO_PIO1_3
+	  __IO uint32_t SWDIO_PIO1_3;
+	  __IO uint32_t ARM_SWDIO_PIO1_3;
+  };
+
   __IO uint32_t PIO1_4;
   __IO uint32_t PIO1_11;
   __IO uint32_t PIO3_2;
@@ -270,8 +291,15 @@ typedef struct
   __IO uint32_t PIO1_6;
   __IO uint32_t PIO1_7;
   __IO uint32_t PIO3_3;
-  __IO uint32_t SCKLOC;   /* For HB1 only, new feature */
-} IOCON_TypeDef;
+  union {
+	  // SCK_LOC preferred name rather than original SCKLOC
+	  __IO uint32_t SCK_LOC;                /*!< Offset: 0x0B0 SCK pin location select Register (R/W) */
+	  __IO uint32_t SCKLOC;
+  };
+  __IO uint32_t DSR_LOC;                /*!< Offset: 0x0B4 DSR pin location select Register (R/W) */
+  __IO uint32_t DCD_LOC;                /*!< Offset: 0x0B8 DCD pin location select Register (R/W) */
+  __IO uint32_t RI_LOC;                 /*!< Offset: 0x0BC RI pin location Register (R/W) */
+} LPC_IOCON_TypeDef;
 
 
 /*------------- Power Management Unit (PMU) --------------------------*/
@@ -283,7 +311,7 @@ typedef struct
   __IO uint32_t GPREG2;
   __IO uint32_t GPREG3;
   __IO uint32_t GPREG4;
-} PMU_TypeDef;
+} LPC_PMU_TypeDef;
 
 
 /*------------- General Purpose Input/Output (GPIO) --------------------------*/
@@ -305,7 +333,7 @@ typedef struct
   __IO uint32_t RIS;
   __IO uint32_t MIS;
   __IO uint32_t IC;
-} GPIO_TypeDef;
+} LPC_GPIO_TypeDef;
 
 
 /*------------- Timer (TMR) --------------------------------------------------*/
@@ -328,7 +356,7 @@ typedef struct
        uint32_t RESERVED2[12];
   __IO uint32_t CTCR;
   __IO uint32_t PWMC;
-} TMR_TypeDef;
+} LPC_TMR_TypeDef;
 
 /*------------- Universal Asynchronous Receiver Transmitter (UART) -----------*/
 typedef struct
@@ -360,7 +388,8 @@ typedef struct
   __IO uint32_t  RS485CTRL;
   __IO uint32_t  ADRMATCH;
   __IO uint32_t  RS485DLY;
-} UART_TypeDef;
+  __I  uint32_t  FIFOLVL;
+} LPC_UART_TypeDef;
 
 /*------------- Synchronous Serial Communication (SSP) -----------------------*/
 typedef struct
@@ -374,7 +403,7 @@ typedef struct
   __IO uint32_t RIS;
   __IO uint32_t MIS;
   __IO uint32_t ICR;
-} SSP_TypeDef;
+} LPC_SSP_TypeDef;
 
 /*------------- Inter-Integrated Circuit (I2C) -------------------------------*/
 typedef struct
@@ -395,7 +424,7 @@ typedef struct
   __IO uint32_t MASK1;
   __IO uint32_t MASK2;
   __IO uint32_t MASK3;
-} I2C_TypeDef;
+} LPC_I2C_TypeDef;
 
 /*------------- Watchdog Timer (WDT) -----------------------------------------*/
 typedef struct
@@ -404,7 +433,11 @@ typedef struct
   __IO uint32_t TC;
   __O  uint32_t FEED;
   __I  uint32_t TV;
-} WDT_TypeDef;
+  uint32_t RESERVED0;
+__IO uint32_t WARNINT;				/*!< Offset: 0x014 Watchdog timer warning int. register (R/W) */
+__IO uint32_t WINDOW;				/*!< Offset: 0x018 Watchdog timer window value register (R/W) */
+
+} LPC_WDT_TypeDef;
 
 /*------------- Analog-to-Digital Converter (ADC) ----------------------------*/
 typedef struct
@@ -413,16 +446,21 @@ typedef struct
   __IO uint32_t GDR;
        uint32_t RESERVED0;
   __IO uint32_t INTEN;
-  __I  uint32_t DR0;
-  __I  uint32_t DR1;
-  __I  uint32_t DR2;
-  __I  uint32_t DR3;
-  __I  uint32_t DR4;
-  __I  uint32_t DR5;
-  __I  uint32_t DR6;
-  __I  uint32_t DR7;
-  __I  uint32_t STAT;
-} ADC_TypeDef;
+  union {
+	  __IO uint32_t DR[8];
+	  struct {
+	  __I  uint32_t DR0;
+	  __I  uint32_t DR1;
+	  __I  uint32_t DR2;
+	  __I  uint32_t DR3;
+	  __I  uint32_t DR4;
+	  __I  uint32_t DR5;
+	  __I  uint32_t DR6;
+	  __I  uint32_t DR7;
+	  };
+  };
+	  __I  uint32_t STAT;
+} LPC_ADC_TypeDef;
 
 
 /*------------- Universal Serial Bus (USB) -----------------------------------*/
@@ -442,64 +480,69 @@ typedef struct
   __O  uint32_t TxPLen;
   __IO uint32_t Ctrl;
   __O  uint32_t DevFIQSel;
-} USB_TypeDef;
+} LPC_USB_TypeDef;
 
 #if defined ( __CC_ARM   )
 #pragma no_anon_unions
 #endif
 
+
 /******************************************************************************/
 /*                         Peripheral memory map                              */
 /******************************************************************************/
 /* Base addresses                                                             */
-#define FLASH_BASE            (0x00000000UL)
-#define RAM_BASE              (0x10000000UL)
-#define APB0_BASE             (0x40000000UL)
-#define AHB_BASE              (0x50000000UL)
+#define LPC_FLASH_BASE        (0x00000000UL)
+#define LPC_RAM_BASE          (0x10000000UL)
+#define LPC_APB0_BASE         (0x40000000UL)
+#define LPC_AHB_BASE          (0x50000000UL)
 
-/* APB0 peripherals                                                          */
-#define I2C_BASE              (APB0_BASE + 0x00000)
-#define WDT_BASE              (APB0_BASE + 0x04000)
-#define UART_BASE             (APB0_BASE + 0x08000)
-#define CT16B0_BASE           (APB0_BASE + 0x0C000)
-#define CT16B1_BASE           (APB0_BASE + 0x10000)
-#define CT32B0_BASE           (APB0_BASE + 0x14000)
-#define CT32B1_BASE           (APB0_BASE + 0x18000)
-#define ADC_BASE              (APB0_BASE + 0x1C000)
-#define USB_BASE              (APB0_BASE + 0x20000)
-#define PMU_BASE              (APB0_BASE + 0x38000)
-#define SSP_BASE              (APB0_BASE + 0x40000)
-#define IOCON_BASE            (APB0_BASE + 0x44000)
-#define SYSCON_BASE           (APB0_BASE + 0x48000)
+/* APB0 peripherals                                                           */
+#define LPC_I2C_BASE          (LPC_APB0_BASE + 0x00000)
+#define LPC_WDT_BASE          (LPC_APB0_BASE + 0x04000)
+#define LPC_UART_BASE         (LPC_APB0_BASE + 0x08000)
+#define LPC_CT16B0_BASE       (LPC_APB0_BASE + 0x0C000)
+#define LPC_CT16B1_BASE       (LPC_APB0_BASE + 0x10000)
+#define LPC_CT32B0_BASE       (LPC_APB0_BASE + 0x14000)
+#define LPC_CT32B1_BASE       (LPC_APB0_BASE + 0x18000)
+#define LPC_ADC_BASE          (LPC_APB0_BASE + 0x1C000)
+#define LPC_USB_BASE          (LPC_APB0_BASE + 0x20000)
+#define LPC_PMU_BASE          (LPC_APB0_BASE + 0x38000)
+// LPC_SSP_BASE original name, LPC_SSP0_BASE new preferred name
+#define LPC_SSP_BASE          (LPC_APB0_BASE + 0x40000)
+#define LPC_SSP0_BASE         (LPC_APB0_BASE + 0x40000)
+#define LPC_IOCON_BASE        (LPC_APB0_BASE + 0x44000)
+#define LPC_SYSCON_BASE       (LPC_APB0_BASE + 0x48000)
+#define LPC_SSP1_BASE         (LPC_APB0_BASE + 0x58000)
 
-
-/* AHB peripherals */	
-#define GPIO_BASE             (AHB_BASE  + 0x00000)
-#define GPIO0_BASE            (AHB_BASE  + 0x00000)
-#define GPIO1_BASE            (AHB_BASE  + 0x10000)
-#define GPIO2_BASE            (AHB_BASE  + 0x20000)
-#define GPIO3_BASE            (AHB_BASE  + 0x30000)
+/* AHB peripherals                                                            */	
+#define LPC_GPIO_BASE         (LPC_AHB_BASE  + 0x00000)
+#define LPC_GPIO0_BASE        (LPC_AHB_BASE  + 0x00000)
+#define LPC_GPIO1_BASE        (LPC_AHB_BASE  + 0x10000)
+#define LPC_GPIO2_BASE        (LPC_AHB_BASE  + 0x20000)
+#define LPC_GPIO3_BASE        (LPC_AHB_BASE  + 0x30000)
 
 /******************************************************************************/
 /*                         Peripheral declaration                             */
 /******************************************************************************/
-#define LPC_I2C                   ((      I2C_TypeDef *)       I2C_BASE)
-#define LPC_WDT                   ((      WDT_TypeDef *)       WDT_BASE)
-#define LPC_UART                  ((     UART_TypeDef *)      UART_BASE)
-#define LPC_TMR16B0               ((      TMR_TypeDef *)    CT16B0_BASE)
-#define LPC_TMR16B1               ((      TMR_TypeDef *)    CT16B1_BASE)
-#define LPC_TMR32B0               ((      TMR_TypeDef *)    CT32B0_BASE)
-#define LPC_TMR32B1               ((      TMR_TypeDef *)    CT32B1_BASE)
-#define LPC_ADC                   ((      ADC_TypeDef *)       ADC_BASE)
-#define LPC_PMU                   ((      PMU_TypeDef *)       PMU_BASE)
-#define LPC_SSP                   ((      SSP_TypeDef *)       SSP_BASE)
-#define LPC_IOCON                 ((    IOCON_TypeDef *)     IOCON_BASE)
-#define LPC_SYSCON                ((   SYSCON_TypeDef *)    SYSCON_BASE)
-#define LPC_USB                   ((      USB_TypeDef *)       USB_BASE)
-
-#define LPC_GPIO0                 ((     GPIO_TypeDef *)     GPIO0_BASE)
-#define LPC_GPIO1                 ((     GPIO_TypeDef *)     GPIO1_BASE)
-#define LPC_GPIO2                 ((     GPIO_TypeDef *)     GPIO2_BASE)
-#define LPC_GPIO3                 ((     GPIO_TypeDef *)     GPIO3_BASE)
+#define LPC_I2C               ((LPC_I2C_TypeDef    *) LPC_I2C_BASE   )
+#define LPC_WDT               ((LPC_WDT_TypeDef    *) LPC_WDT_BASE   )
+#define LPC_UART              ((LPC_UART_TypeDef   *) LPC_UART_BASE  )
+#define LPC_TMR16B0           ((LPC_TMR_TypeDef    *) LPC_CT16B0_BASE)
+#define LPC_TMR16B1           ((LPC_TMR_TypeDef    *) LPC_CT16B1_BASE)
+#define LPC_TMR32B0           ((LPC_TMR_TypeDef    *) LPC_CT32B0_BASE)
+#define LPC_TMR32B1           ((LPC_TMR_TypeDef    *) LPC_CT32B1_BASE)
+#define LPC_ADC               ((LPC_ADC_TypeDef    *) LPC_ADC_BASE   )
+#define LPC_PMU               ((LPC_PMU_TypeDef    *) LPC_PMU_BASE   )
+// LPC_SSP original name, LPC_SSP0 new preferred name
+#define LPC_SSP               ((LPC_SSP_TypeDef    *) LPC_SSP_BASE   )
+#define LPC_SSP0              ((LPC_SSP_TypeDef    *) LPC_SSP0_BASE  )
+#define LPC_SSP1              ((LPC_SSP_TypeDef    *) LPC_SSP1_BASE  )
+#define LPC_IOCON             ((LPC_IOCON_TypeDef  *) LPC_IOCON_BASE )
+#define LPC_SYSCON            ((LPC_SYSCON_TypeDef *) LPC_SYSCON_BASE)
+#define LPC_USB               ((LPC_USB_TypeDef    *) LPC_USB_BASE   )
+#define LPC_GPIO0             ((LPC_GPIO_TypeDef   *) LPC_GPIO0_BASE )
+#define LPC_GPIO1             ((LPC_GPIO_TypeDef   *) LPC_GPIO1_BASE )
+#define LPC_GPIO2             ((LPC_GPIO_TypeDef   *) LPC_GPIO2_BASE )
+#define LPC_GPIO3             ((LPC_GPIO_TypeDef   *) LPC_GPIO3_BASE )
 
 #endif  // __LPC13xx_H__
